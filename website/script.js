eb12da4033516b07165099b2888bfb334138cb48
czhang2718 <czhang2718@gmail.com>
2024-08-06 20:47:59 -0700

    create real a,b input

--- website/script.js
+++ website/script.js
@@ -2,3 +2,9 @@ function changeTextColor() {
     const content = document.getElementById("content");
     content.style.color = content.style.color === "black" ? "green" : "black";
 }
+
+function prove() {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    console.log(a, b);
+}
