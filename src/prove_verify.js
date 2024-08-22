175ca849aa67be0bd0091b87255c03955840627a
janabel <janabeltxia@gmail.com>
2024-08-22 12:17:20 -0700

    add vkey box to display

--- src/prove_verify.js
+++ src/prove_verify.js
@@ -1,7 +1,11 @@
-import { generateSnarkMessageHash, hexToBigInt, fromHexString } from "@pcd/util";
-import { buildEddsa } from 'circomlibjs';
+import {
+  generateSnarkMessageHash,
+  hexToBigInt,
+  fromHexString,
+} from "@pcd/util";
+import { buildEddsa } from "circomlibjs";
 import { groth16 } from "snarkjs";
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
 
 let proof = undefined;
 let publicSignals = undefined;
@@ -11,8 +15,10 @@ const vkey = await vkeyResponse.json();
 const STATIC_ZK_EDDSA_FROG_PCD_NULLIFIER = generateSnarkMessageHash(
   "hard-coded-zk-eddsa-frog-pcd-nullifier"
 );
-const CZ_SEMPAHORE_ID_TRAPDOOR = "135040283343710365777958365424694852760089427911973547434460426204380274744";
-const CZ_SEMPAHORE_ID_NULLIFIER = "358655312360435269311557940631516683613039221013826685666349061378483316589";
+const CZ_SEMPAHORE_ID_TRAPDOOR =
+  "135040283343710365777958365424694852760089427911973547434460426204380274744";
+const CZ_SEMPAHORE_ID_NULLIFIER =
+  "358655312360435269311557940631516683613039221013826685666349061378483316589";
 
 // Check if the object has the required properties and their types
 function isValidProof(parsed) {
@@ -71,141 +77,147 @@ async function parseFrog(rawFrog, semaphoreIDtrapdoor, semaphoreIDnullifier) {
     "reservedField3": "0"
   } */
 
-  console.log("semaphoreIDtrapdoor", semaphoreIDtrapdoor)
-  const rawJSON = JSON.parse(rawFrog)
+  console.log("semaphoreIDtrapdoor", semaphoreIDtrapdoor);
+  const rawJSON = JSON.parse(rawFrog);
 
-  assert(rawJSON.type == "eddsa-frog-pcd")
-  const frogPCD = rawJSON.pcd
-  const frogJSON = JSON.parse(frogPCD)
-  const eddsaPCD = frogJSON.eddsaPCD
-  const frogData = frogJSON.data
-  const { name, description, imageUrl,...frogInfo } = frogData;
+  assert(rawJSON.type == "eddsa-frog-pcd");
+  const frogPCD = rawJSON.pcd;
+  const frogJSON = JSON.parse(frogPCD);
+  const eddsaPCD = frogJSON.eddsaPCD;
+  const frogData = frogJSON.data;
+  const { name, description, imageUrl, ...frogInfo } = frogData;
 
-  const pcdJSON= JSON.parse(eddsaPCD.pcd)
-  
-  assert(pcdJSON.type == "eddsa-pcd")
-  const signature = pcdJSON.proof.signature
+  const pcdJSON = JSON.parse(eddsaPCD.pcd);
+
+  assert(pcdJSON.type == "eddsa-pcd");
+  const signature = pcdJSON.proof.signature;
 
   const eddsa = await buildEddsa();
-  const rawSig = eddsa.unpackSignature(
-      fromHexString(signature)
-  );
+  const rawSig = eddsa.unpackSignature(fromHexString(signature));
   const frogSignatureR8x = eddsa.F.toObject(rawSig.R8[0]).toString();
   const frogSignatureR8y = eddsa.F.toObject(rawSig.R8[1]).toString();
   const frogSignatureS = rawSig.S.toString();
 
-  return {...frogInfo, 
-    "frogSignerPubkeyAx": hexToBigInt(pcdJSON.claim.publicKey[0]).toString(),
-    "frogSignerPubkeyAy": hexToBigInt(pcdJSON.claim.publicKey[1]).toString(),
-    "semaphoreIdentityTrapdoor": semaphoreIDtrapdoor,
-    "semaphoreIdentityNullifier": semaphoreIDnullifier,
-    "watermark": "2718", // ?
-    "frogSignatureR8x": frogSignatureR8x,
-    "frogSignatureR8y": frogSignatureR8y,
-    "frogSignatureS": frogSignatureS,
-    "externalNullifier": STATIC_ZK_EDDSA_FROG_PCD_NULLIFIER,
-    "reservedField1": "0",
-    "reservedField2": "0",
-    "reservedField3": "0"
-  }
+  return {
+    ...frogInfo,
+    frogSignerPubkeyAx: hexToBigInt(pcdJSON.claim.publicKey[0]).toString(),
+    frogSignerPubkeyAy: hexToBigInt(pcdJSON.claim.publicKey[1]).toString(),
+    semaphoreIdentityTrapdoor: semaphoreIDtrapdoor,
+    semaphoreIdentityNullifier: semaphoreIDnullifier,
+    watermark: "2718", // ?
+    frogSignatureR8x: frogSignatureR8x,
+    frogSignatureR8y: frogSignatureR8y,
+    frogSignatureS: frogSignatureS,
+    externalNullifier: STATIC_ZK_EDDSA_FROG_PCD_NULLIFIER,
+    reservedField1: "0",
+    reservedField2: "0",
+    reservedField3: "0",
+  };
 }
 
-document.getElementById("prove-button").addEventListener("click", async function () {
-  const frogInput = document.getElementById("frog-input").value;
-  const semaphoreTrap = document.getElementById("id-trapdoor").value || CZ_SEMPAHORE_ID_TRAPDOOR;
-  const semaphoreNull = document.getElementById("id-nullifier").value || CZ_SEMPAHORE_ID_NULLIFIER;
-  console.log("semaphoreID0", semaphoreTrap)
+document
+  .getElementById("prove-button")
+  .addEventListener("click", async function () {
+    const frogInput = document.getElementById("frog-input").value;
+    const semaphoreTrap =
+      document.getElementById("id-trapdoor").value || CZ_SEMPAHORE_ID_TRAPDOOR;
+    const semaphoreNull =
+      document.getElementById("id-nullifier").value ||
+      CZ_SEMPAHORE_ID_NULLIFIER;
+    console.log("semaphoreID0", semaphoreTrap);
 
+    const circuitInputs = await parseFrog(
+      frogInput,
+      semaphoreTrap,
+      semaphoreNull
+    );
 
-  const circuitInputs = await parseFrog(frogInput, semaphoreTrap, semaphoreNull);
+    console.log("circuitInputs", circuitInputs);
 
-  console.log("circuitInputs", circuitInputs);
+    try {
+      ({ proof, publicSignals } = await groth16.fullProve(
+        circuitInputs,
+        "./frog.wasm",
+        "./frog_final.zkey"
+      ));
+      console.log("publicSignals", publicSignals);
+      console.log("proof", proof);
+    } catch (error) {
+      alert(`Invalid signature or frog.\n${error.message}`);
+    }
 
-  try {
-    ({ proof, publicSignals } = await groth16.fullProve(
-      circuitInputs,
-      "./frog.wasm",
-      "./frog_final.zkey"
-    ));
-    console.log("publicSignals", publicSignals);
-    console.log("proof", proof);
-  } catch (error) {
-    alert(`Invalid signature or frog.\n${error.message}`);
-  }
-  
+    let resultHeader = document.createElement("h2");
+    resultHeader.innerText = "Result";
 
-  
-  let resultHeader = document.createElement("h2");
-  resultHeader.innerText = "Result";
+    let oldProofBox = document.createElement("text-box");
+    oldProofBox.innerText =
+      "Proof: " +
+      JSON.stringify(proof) +
+      "\n" +
+      "Public Signals: " +
+      publicSignals.toString();
 
-  let oldProofBox = document.createElement("text-box");
-  oldProofBox.innerText =
-    "Proof: " +
-    JSON.stringify(proof) +
-    "\n" +
-    "Public Signals: " +
-    publicSignals.toString();
+    let proofBox = document.createElement("textarea");
+    proofBox.innerHTML = JSON.stringify(proof, null, 2);
 
-  let proofBox = document.createElement("textarea");
-  proofBox.innerHTML = JSON.stringify(proof, null, 2);
+    let publicSignalsBox = document.createElement("textarea");
+    publicSignalsBox.innerHTML = JSON.stringify(publicSignals, null, 2);
 
-  let publicSignalsBox = document.createElement("textarea");
-  publicSignalsBox.innerHTML = JSON.stringify(publicSignals, null, 2);
+    let vkeyBox = document.createElement("textarea");
+    vkeyBox.innerHTML = JSON.stringify(vkey, null, 2);
 
-  const proofResult = document.getElementById("proof-result");
-  proofResult.appendChild(resultHeader);
-  // proofResult.appendChild(document.createTextNode("Proof:\n\n"));
-  proofResult.appendChild(proofBox);
-  // proofResult.appendChild(document.createTextNode("Public Signals:\n"));
-  proofResult.appendChild(publicSignalsBox);
-  // proofResult.appendChild(oldProofBox);
-});
+    const proofResult = document.getElementById("proof-result");
+    proofResult.appendChild(resultHeader);
+    proofResult.appendChild(proofBox);
+    proofResult.appendChild(publicSignalsBox);
+    proofResult.appendChild(vkeyBox);
+  });
 
+document
+  .getElementById("verify-proof-button")
+  .addEventListener("click", async function () {
+    if (!publicSignals) {
+      console.log("did not provide input values yet");
+      return;
+    }
+    const verifyResult = document.getElementById("verify-result");
 
+    console.log("button clicked!");
+    const rawProof = document.getElementById("proof").value;
 
-document.getElementById("verify-proof-button").addEventListener("click", async function () {
-  if (!publicSignals) {
-    console.log("did not provide input values yet");
-    return;
-  }
-  const verifyResult = document.getElementById("verify-result");
+    // checking that the proof is a JSON
+    let proof = undefined;
+    try {
+      proof = JSON.parse(rawProof);
+      console.log(proof);
+      console.log("got valid json");
+    } catch (error) {
+      verifyResult.innerText = "Please enter a valid proof!";
+      console.log("caught an error:", error.message);
+      return;
+    }
 
-  console.log("button clicked!");
-  const rawProof = document.getElementById("proof").value;
+    // checking that the proof is a JSON of the correct proof format
+    if (!isValidProof(proof)) {
+      verifyResult.innerText = "Please enter a valid proof!";
+      return;
+    }
 
-  // checking that the proof is a JSON
-  let proof = undefined;
-  try {
-    proof = JSON.parse(rawProof);
-    console.log(proof);
-    console.log("got valid json");
-  } catch (error) {
-    verifyResult.innerText = "Please enter a valid proof!";
-    console.log("caught an error:", error.message);
-    return;
-  }
+    // trying to verify the proof, throws and error if groth16.verify throws and error
+    let verify = undefined;
+    try {
+      console.log("vkey", vkey, "publicSignals", publicSignals, "proof", proof);
+      verify = await groth16.verify(vkey, publicSignals, proof);
+      console.log("successfully verified...");
+    } catch (error) {
+      verifyResult.innerText = "Error verifying proof...";
+      return;
+    }
 
-  // checking that the proof is a JSON of the correct proof format
-  if (!isValidProof(proof)) {
-    verifyResult.innerText = "Please enter a valid proof!";
-    return;
-  }
-
-  // trying to verify the proof, throws and error if groth16.verify throws and error
-  let verify = undefined;
-  try {
-    console.log("vkey", vkey, "publicSignals", publicSignals, "proof", proof);
-    verify = await groth16.verify(vkey, publicSignals, proof);
-    console.log("successfully verified...");
-  } catch (error) {
-    verifyResult.innerText = "Error verifying proof...";
-    return;
-  }
-
-  // update text of verifyResult div with status of verification
-  if (verify) {
-    verifyResult.innerText = "Proof was verified!";
-  } else {
-    verifyResult.innerText = "Proof was rejected :(";
-  }
-});
+    // update text of verifyResult div with status of verification
+    if (verify) {
+      verifyResult.innerText = "Proof was verified!";
+    } else {
+      verifyResult.innerText = "Proof was rejected :(";
+    }
+  });
