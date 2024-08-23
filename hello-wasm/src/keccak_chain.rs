668d68c51d422eb9047f37a34e4369ba58ed89c4
czhang2718 <czhang2718@gmail.com>
2024-08-22 17:08:49 -0700

    extract public inputs and add alert stuff

--- hello-wasm/src/keccak_chain.rs
+++ hello-wasm/src/keccak_chain.rs
@@ -1,4 +1,5 @@
 use web_sys::console;
+use wasm_bindgen::prelude::*;
 use console_error_panic_hook;
 use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
 use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
@@ -9,7 +10,6 @@ use ark_ff::PrimeField;
 
 use std::path::PathBuf;
 use std::rc::Rc;
-use std::time::Instant;
 
 use folding_schemes::{
     utils::PathOrBin,
@@ -46,22 +46,27 @@ pub fn rust_native_step<F: PrimeField>(
     bytes_to_f_vec_bits(z_i1.to_vec())
 }
 
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
 pub fn full_flow(r1cs_bytes: Vec<u8>, wasm_bytes: Vec<u8>) -> String {
     console_error_panic_hook::set_once();
 
 
     // set how many steps of folding we want to compute
-    let n_steps = 5;
+    let n_steps = 2;
 
     // set the initial state
     let z_0_aux: Vec<u32> = vec![0_u32; 32 * 8];
     let z_0: Vec<Fr> = z_0_aux.iter().map(|v| Fr::from(*v)).collect::<Vec<Fr>>();
 
-    let r1cs_bin = PathOrBin::from(r1cs_bytes);
-    let wasm_bin = PathOrBin::from(wasm_bytes);
+    // let r1cs_bin = PathOrBin::from(r1cs_bytes);
+    // let wasm_bin = PathOrBin::from(wasm_bytes);
 
     // initialize the Circom circuit
-    let f_circuit_params = (r1cs_bin, wasm_bin, 32 * 8, 0);
+    let f_circuit_params = (r1cs_bytes.into(), wasm_bytes.into(), 32 * 8, 0);
     let mut f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
 
     // Note (optional): for more speed, we can set a custom rust-native logic, which will be
@@ -70,57 +75,10 @@ pub fn full_flow(r1cs_bytes: Vec<u8>, wasm_bytes: Vec<u8>) -> String {
     f_circuit.set_custom_step_native(Rc::new(rust_native_step));
 
     // return format!("hi");
+    alert("step native");
     // everything above^ works
 
-    // ----------------
-    // Sanity check
-    // check that the f_circuit produces valid R1CS constraints
-    use ark_r1cs_std::alloc::AllocVar;
-    use ark_r1cs_std::fields::fp::FpVar;
-    use ark_r1cs_std::R1CSVar;
-    use ark_relations::r1cs::ConstraintSystem;
-    let cs = ConstraintSystem::<Fr>::new_ref();
-    let z_0_var = Vec::<FpVar<Fr>>::new_witness(cs.clone(), || Ok(z_0.clone())).unwrap();
-
-    match f_circuit.generate_step_constraints(cs.clone(), 1, z_0_var, vec![]) {
-        Ok(z_1_var) => {
-            return format!("hi");
-        },
-        Err(e) => {
-            // THIS PRODUCES ERROR
-            // hello_wasm.js:368 panicked at /home/czhang2718/.cargo/git/checkouts/circom-compat-69e71b86d9777e3c/18ca7f2/src/witness/circom.rs:158:33:
-            // function getFrLen not found
-
-            // Stack:
-
-            // Error
-            //     at imports.wbg.__wbg_new_abda76e883ba8a5f (http://localhost:8000/pkg/hello_wasm.js:352:21)
-            //     at http://localhost:8000/pkg/hello_wasm_bg.wasm:wasm-function[490]:0x43613
-            //     at http://localhost:8000/pkg/hello_wasm_bg.wasm:wasm-function[187]:0x38bae
-            //     at http://localhost:8000/pkg/hello_wasm_bg.wasm:wasm-function[332]:0x3f180
-            //     at http://localhost:8000/pkg/hello_wasm_bg.wasm:wasm-function[139]:0x2d22d
-            //     at http://localhost:8000/pkg/hello_wasm_bg.wasm:wasm-function[53]:0x14939
-            //     at run_keccak (http://localhost:8000/pkg/hello_wasm.js:226:10)
-            //     at http://localhost:8000/:25:9
-            web_sys::console::error_1(&format!("Error generating step constraints: {:?}", e).into());
-            panic!("Failed to generate step constraints");
-        },
-    }
-    let z_1_var = f_circuit
-        .generate_step_constraints(cs.clone(), 1, z_0_var, vec![])
-        .unwrap();
     
-    // check z_1_var against the native z_1
-    let z_1_native = f_circuit.step_native(1, z_0.clone(), vec![]).unwrap();
-    assert_eq!(z_1_var.value().unwrap(), z_1_native);
-    // check that the constraint system is satisfied
-    assert!(cs.is_satisfied().unwrap());
-    // ----------------
-
-    // return "good"
-
-    // define type aliases for the FoldingScheme (FS) and Decider (D), to avoid writting the
-    // whole type each time
     pub type FS =
         Nova<G1, GVar, G2, GVar2, CircomFCircuit<Fr>, KZG<'static, Bn254>, Pedersen<G2>, false>;
     pub type D = DeciderEth<
@@ -138,85 +96,60 @@ pub fn full_flow(r1cs_bytes: Vec<u8>, wasm_bytes: Vec<u8>) -> String {
     let poseidon_config = poseidon_canonical_config::<Fr>();
     let mut rng = rand::rngs::OsRng;
 
+    alert("rng");
+
     // prepare the Nova prover & verifier params
     let nova_preprocess_params = PreprocessorParam::new(poseidon_config, f_circuit.clone());
 
-    let start = Instant::now();
-    let nova_params = FS::preprocess(&mut rng, &nova_preprocess_params).unwrap();
+    alert("nova_preprocess_params");
 
+    let nova_params = FS::preprocess(&mut rng, &nova_preprocess_params).unwrap();
+    alert("nova params");
     
 
     // initialize the folding scheme engine, in our case we use Nova
-    // let mut nova = FS::init(&nova_params, f_circuit.clone(), z_0.clone()).unwrap();
+    let mut nova = FS::init(&nova_params, f_circuit.clone(), z_0.clone()).unwrap();
 
-    // // run n steps of the folding iteration
-    // let start_full = Instant::now();
-    // for _ in 0..n_steps {
-    //     let start = Instant::now();
-    //     nova.prove_step(rng, vec![], None).unwrap();
-    //     println!(
-    //         "Nova::prove_step (keccak256 through Circom) {}: {:?}",
-    //         nova.i,
-    //         start.elapsed()
-    //     );
-    // }
-    // println!(
-    //     "Nova's all {} steps time: {:?}",
-    //     n_steps,
-    //     start_full.elapsed()
-    // );
-    // return format!(
-    //     "Nova's all {} steps time: {:?}",
-    //     n_steps,
-    //     start_full.elapsed()
-    // );
+    // return format!("hi");
 
-    // // perform the hash chain natively in rust (which uses a rust Keccak256 library)
-    // let mut z_i_native = z_0.clone();
-    // for i in 0..n_steps {
-    //     z_i_native = rust_native_step(i, z_i_native, vec![]).unwrap();
-    // }
-    // // check that the value of the last folding state (nova.z_i) computed through folding, is
-    // // equal to the natively computed hash using the rust_native_step method
-    // assert_eq!(nova.z_i, z_i_native);
+    // run n steps of the folding iteration
+    for _ in 0..n_steps {
+        nova.prove_step(rng, vec![], None).unwrap();
+        alert(format!("Nova::prove_step {}", i));
+    }
 
-    // // ----------------
-    // // Sanity check
-    // // The following lines contain a sanity check that checks the IVC proof (before going into
-    // // the zkSNARK proof)
-    // let (running_instance, incoming_instance, cyclefold_instance) = nova.instances();
-    // FS::verify(
-    //     nova_params.1.clone(), // Nova's verifier params
-    //     z_0,
-    //     nova.z_i.clone(),
-    //     nova.i,
-    //     running_instance,
-    //     incoming_instance,
-    //     cyclefold_instance,
-    // )
-    // .unwrap();
-    // // ----------------
+    // prepare the Decider prover & verifier params
+    let (decider_pp, decider_vp) = D::preprocess(&mut rng, &nova_params, nova.clone()).unwrap();
 
-    // // prepare the Decider prover & verifier params
-    // let start = Instant::now();
-    // let (decider_pp, decider_vp) = D::preprocess(&mut rng, &nova_params, nova.clone()).unwrap();
-    // println!("Decider params generated: {:?}", start.elapsed());
+    alert("D preprocess");
 
-    // let rng = rand::rngs::OsRng;
-    // let start = Instant::now();
-    // let proof = D::prove(rng, decider_pp, nova.clone()).unwrap();
-    // println!("generated Decider proof: {:?}", start.elapsed());
+    let rng = rand::rngs::OsRng;
+    let proof = D::prove(rng, decider_pp, nova.clone()).unwrap();
 
-    // let verified = D::verify(
-    //     decider_vp.clone(),
-    //     nova.i,
-    //     nova.z_0.clone(),
-    //     nova.z_i.clone(),
-    //     &nova.U_i,
-    //     &nova.u_i,
-    //     &proof,
-    // )
-    // .unwrap();
-    // assert!(verified);
-    // println!("Decider proof verification: {}", verified);
+    let verified = D::verify(
+        decider_vp.clone(),
+        nova.i,
+        nova.z_0.clone(),
+        nova.z_i.clone(),
+        &nova.U_i,
+        &nova.u_i,
+        &proof,
+    )
+    .unwrap();
+    assert!(verified);
+    alert(format!("Decider proof verification: {}", verified));
+
+    let public_input = D::get_public_input(
+        decider_vp.clone(),
+        nova.i,
+        nova.z_0.clone(),
+        nova.z_i.clone(),
+        &nova.U_i,
+        &nova.u_i,
+        &proof,
+    )
+    .unwrap();
+    alert("DONE");
+    alert(&format!("{:?}", public_input).into());
+    return format!("hi");
 }
