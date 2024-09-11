3a0b05bb2372a0d927f460ae11de3de8b63901ce
janabel <janabeltxia@gmail.com>
2024-09-11 00:06:33 -0400

    add circuit builds to git

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,73 @@
+// // FROG FOLDING!
+
+// #![allow(non_snake_case)]
+// #![allow(non_camel_case_types)]
+// #![allow(clippy::upper_case_acronyms)]
+// // use ark_ff::MontBackend;
+// ///
+// /// This example performs the full flow:
+// /// - define the circuit to be folded
+// /// - fold the circuit with Nova+CycleFold's IVC
+// /// - generate a DeciderEthCircuit final proof
+// /// - generate the Solidity contract that verifies the proof
+// /// - verify the proof in the EVM
+// ///
+// /// 
+
+// // use ethereum_types::U256;
+// use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
+
+// use ark_groth16::Groth16;
+// // use ark_groth16::{Groth16, Proof};
+// use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
+
+// use num_bigint::BigInt;
+// use num_traits::Num;
+// use ark_ff::BigInteger256;
+
+// use std::path::PathBuf;
+// use std::time::Instant;
+
+// use folding_schemes::{
+//     commitment::{kzg::KZG, pedersen::Pedersen},
+//     folding::nova::{
+//         decider_eth::{prepare_calldata, Decider as DeciderEth},
+//         Nova, PreprocessorParam,
+//     },
+//     frontend::{circom::CircomFCircuit, FCircuit},
+//     transcript::poseidon::poseidon_canonical_config,
+//     Decider, FoldingScheme,
+// };
+// use solidity_verifiers::{
+//     evm::{compile_solidity, Evm},
+//     utils::get_function_selector_for_nova_cyclefold_verifier,
+//     verifiers::nova_cyclefold::get_decider_template_for_cyclefold_decider,
+//     NovaCycleFoldVerifierKey,
+// };
+
+// use std::fs;
+// // use std::io::Result;
+// use serde::Deserialize;
+// // use serde_json::Value;
+// use std::collections::HashMap;
+
+
 // FROG FOLDING!
 
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(clippy::upper_case_acronyms)]
+// use ark_bn254::Bn254;
+use ark_crypto_primitives::sponge::Absorb;
+use ark_ec::{AffineRepr, CurveGroup, Group};
+use ark_ff::{BigInteger, PrimeField};
+// use ark_groth16::Groth16;
+use ark_r1cs_std::{groups::GroupOpsBounds, prelude::CurveVar, ToConstraintFieldGadget};
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_snark::SNARK;
+use ark_std::rand::{CryptoRng, RngCore};
+use ark_std::{One, Zero};
+use core::marker::PhantomData;
 // use ark_ff::MontBackend;
 ///
 /// This example performs the full flow:
@@ -13,6 +78,8 @@
 /// - verify the proof in the EVM
 ///
 /// 
+/// 
+// use wasm_bindgen::prelude::wasm_bindgen;
 
 // use ethereum_types::U256;
 use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
@@ -28,21 +95,16 @@ use ark_ff::BigInteger256;
 use std::path::PathBuf;
 use std::time::Instant;
 
+use folding_schemes::folding::nova::CommittedInstance;
 use folding_schemes::{
     commitment::{kzg::KZG, pedersen::Pedersen},
     folding::nova::{
-        decider_eth::{prepare_calldata, Decider as DeciderEth},
+        decider_eth::{prepare_calldata, Decider as DeciderEth, VerifierParam, Proof},
         Nova, PreprocessorParam,
     },
     frontend::{circom::CircomFCircuit, FCircuit},
     transcript::poseidon::poseidon_canonical_config,
     Decider, FoldingScheme,
-};
-use solidity_verifiers::{
-    evm::{compile_solidity, Evm},
-    utils::get_function_selector_for_nova_cyclefold_verifier,
-    verifiers::nova_cyclefold::get_decider_template_for_cyclefold_decider,
-    NovaCycleFoldVerifierKey,
 };
 
 use std::fs;
@@ -201,7 +263,9 @@ fn main() {
     // prepare the Decider prover & verifier params
     let (decider_pp, decider_vp) = D::preprocess(&mut rng, &nova_params, nova.clone()).unwrap();
     println!("{}", "prepared decider prover & verifier params!");
-    println!("{:?}", decider_vp.1);
+    println!("{:?}", decider_vp.pp_hash);
+    println!("{:?}", decider_vp.snark_vp);
+    println!("{:?}", decider_vp.cs_vp);
 
     // run n steps of the folding iteration
     for (i, external_inputs_at_step) in external_inputs.iter().enumerate() {
@@ -243,41 +307,41 @@ fn main() {
     println!("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
 
     // Now, let's generate the Solidity code that verifies this Decider final proof
-    let function_selector =
-        get_function_selector_for_nova_cyclefold_verifier(nova.z_0.len() * 2 + 1);
+    // let function_selector =
+    //     get_function_selector_for_nova_cyclefold_verifier(nova.z_0.len() * 2 + 1);
 
-    let calldata: Vec<u8> = prepare_calldata(
-        function_selector,
-        nova.i,
-        nova.z_0,
-        nova.z_i,
-        &nova.U_i,
-        &nova.u_i,
-        proof,
-    )
-    .unwrap();
+    // let calldata: Vec<u8> = prepare_calldata(
+    //     function_selector,
+    //     nova.i,
+    //     nova.z_0,
+    //     nova.z_i,
+    //     &nova.U_i,
+    //     &nova.u_i,
+    //     proof,
+    // )
+    // .unwrap();
 
-    // prepare the setup params for the solidity verifier
-    let nova_cyclefold_vk = NovaCycleFoldVerifierKey::from((decider_vp, f_circuit.state_len()));
+    // // prepare the setup params for the solidity verifier
+    // let nova_cyclefold_vk = NovaCycleFoldVerifierKey::from((decider_vp, f_circuit.state_len()));
 
-    // generate the solidity code
-    let decider_solidity_code = get_decider_template_for_cyclefold_decider(nova_cyclefold_vk);
+    // // generate the solidity code
+    // let decider_solidity_code = get_decider_template_for_cyclefold_decider(nova_cyclefold_vk);
 
-    // verify the proof against the solidity code in the EVM
-    let nova_cyclefold_verifier_bytecode = compile_solidity(&decider_solidity_code, "NovaDecider");
-    let mut evm = Evm::default();
-    let verifier_address = evm.create(nova_cyclefold_verifier_bytecode);
-    let (_, output) = evm.call(verifier_address, calldata.clone());
-    assert_eq!(*output.last().unwrap(), 1);
+    // // verify the proof against the solidity code in the EVM
+    // let nova_cyclefold_verifier_bytecode = compile_solidity(&decider_solidity_code, "NovaDecider");
+    // let mut evm = Evm::default();
+    // let verifier_address = evm.create(nova_cyclefold_verifier_bytecode);
+    // let (_, output) = evm.call(verifier_address, calldata.clone());
+    // assert_eq!(*output.last().unwrap(), 1);
 
-    // save smart contract and the calldata
-    println!("storing nova-verifier.sol and the calldata into files");
-    fs::write(
-        "./src/nova_solidity_outputs/frogIVC_split-nova-verifier.sol",
-        decider_solidity_code.clone(),
-    )
-    .unwrap();
-    fs::write("./src/nova_solidity_outputs/frogIVC_split-solidity-calldata.calldata", calldata.clone()).unwrap();
-    let s = solidity_verifiers::utils::get_formatted_calldata(calldata.clone());
-    fs::write("./src/nova_solidity_outputs/frogIVC_split-solidity-calldata.inputs", s.join(",\n")).expect("");
+    // // save smart contract and the calldata
+    // println!("storing nova-verifier.sol and the calldata into files");
+    // fs::write(
+    //     "./src/nova_solidity_outputs/frogIVC_split-nova-verifier.sol",
+    //     decider_solidity_code.clone(),
+    // )
+    // .unwrap();
+    // fs::write("./src/nova_solidity_outputs/frogIVC_split-solidity-calldata.calldata", calldata.clone()).unwrap();
+    // let s = solidity_verifiers::utils::get_formatted_calldata(calldata.clone());
+    // fs::write("./src/nova_solidity_outputs/frogIVC_split-solidity-calldata.inputs", s.join(",\n")).expect("");
 }
