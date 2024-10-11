d8eff02939a21a55c9f89c2dd375281185779c8c
janabel <janabeltxia@gmail.com>
2024-10-11 13:23:38 -0400

    add untested ivc prover

--- prover-site/sonobe-rust/src/lib.rs
+++ prover-site/sonobe-rust/src/lib.rs
@@ -23,17 +23,13 @@ fn get_current_time_in_millis() -> f64 {
 }
 
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
 use ark_groth16::{Groth16, VerifyingKey, ProvingKey};
+use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
 use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
 
 use num_bigint::BigInt;
 use num_traits::Num;
 use ark_ff::BigInteger256;
-
-use std::path::PathBuf;
-use std::time::Instant;
-use serde_wasm_bindgen::to_value;
 
 use folding_schemes::commitment::CommitmentScheme;
 use folding_schemes::{
@@ -48,6 +44,9 @@ use folding_schemes::{
     Decider, FoldingScheme,
 };
 
+use std::path::PathBuf;
+use std::time::Instant;
+use serde_wasm_bindgen::to_value;
 use std::fs;
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -92,8 +91,7 @@ struct Frog {
     ownerSemaphoreId: String,
     frogSignerPubkeyAx: String,
     frogSignerPubkeyAy: String,
-    semaphoreIdentityTrapdoor: String,
-    semaphoreIdentityNullifier: String,
+    semaphoreIdentityCommitment: String,
     watermark: String,
     frogSignatureR8x: String,
     frogSignatureR8y: String,
@@ -128,8 +126,7 @@ fn frog_to_fr_vector(frog: &Frog) -> Vec<Fr> {
         str_to_fr(&frog.ownerSemaphoreId),
         str_to_fr(&frog.frogSignerPubkeyAx),
         str_to_fr(&frog.frogSignerPubkeyAy),
-        str_to_fr(&frog.semaphoreIdentityTrapdoor),
-        str_to_fr(&frog.semaphoreIdentityNullifier),
+        str_to_fr(&frog.semaphoreIdentityCommitment),
         str_to_fr(&frog.watermark),
         str_to_fr(&frog.frogSignatureR8x),
         str_to_fr(&frog.frogSignatureR8y),
@@ -154,28 +151,14 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
     frogs_js: JsValue,
     nova_pp_serialized: Vec<u8>,
     nova_vp_serialized: Vec<u8>,
-    g16_vk_serialized: Vec<u8>,
-    g16_pk_serialized: Vec<u8>) {
+    ) {
 
     set_panic_hook();
     // alert("panic hook set!");
 
     pub type N =
         Nova<G1, GVar, G2, GVar2, CircomFCircuit<Fr>, KZG<'static, Bn254>, Pedersen<G2>, false>;
-    pub type D = DeciderEth<
-        G1,
-        GVar,
-        G2,
-        GVar2,
-        CircomFCircuit<Fr>,
-        KZG<'static, Bn254>,
-        Pedersen<G2>,
-        Groth16<Bn254>,
-        N,
-    >;
-
-    // Decider<C1, GC1, C2, GC2, FC, CS1, CS2, S, FS> commented here for type references
-
+    
     let start_total = get_current_time_in_millis();
     
     let poseidon_config = poseidon_canonical_config::<Fr>();
@@ -200,7 +183,7 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
 
     let start = get_current_time_in_millis();
         // (r1cs_bytes, wasm_bytes, state_len, external_inputs_len)
-        let f_circuit_params = (r1cs_bytes.into(), wasm_bytes.into(), 3, 22);
+        let f_circuit_params = (r1cs_bytes.into(), wasm_bytes.into(), 3, 21);
         let mut f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
         
         let end = get_current_time_in_millis();
@@ -210,13 +193,16 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
 
     // deserialize parameters
     let start = get_current_time_in_millis();
-        let nova_pp_deserialized = ProverParams::<
+    let nova_pp_deserialized = ProverParams::<
             G1,
             G2,
             KZG<'static, Bn254>,
             Pedersen<G2>,
-            >::deserialize_uncompressed_unchecked(
-                &mut nova_pp_serialized.as_slice()
+            >::deserialize_with_mode(
+                &mut nova_pp_serialized.as_slice(),
+                ark_serialize::Compress::No,
+                ark_serialize::Validate::No,
+                // (), // fcircuit_params
             )
             .unwrap();
 
@@ -228,12 +214,16 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
 
     let start = get_current_time_in_millis();
         let nova_vp_deserialized = VerifierParams::<
-                G1,
-                G2,
-                KZG<'static, Bn254>,
-                Pedersen<G2>,
-            >::deserialize_uncompressed_unchecked(
-                &mut nova_vp_serialized.as_slice()
+            G1,
+            G2,
+            KZG<'static, Bn254>,
+            Pedersen<G2>,
+            false,
+            >::deserialize_with_mode::<GVar, GVar2, CircomFCircuit<Fr>, _>(
+                &mut nova_vp_serialized.as_slice(),
+                ark_serialize::Compress::No,
+                ark_serialize::Validate::No,
+                f_circuit_params,
             )
             .unwrap();
 
@@ -243,28 +233,7 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
         //  web_sys::console::log_1(&format!("nova_vp_deserialized: {:?}", nova_vp_deserialized).into());
          web_sys::console::log_1(&format!("deserialized nova_vp: {:?}", elapsed).into());
 
-    let start = get_current_time_in_millis();
-        let g16_vk_deserialized: VerifyingKey<Bn254> = VerifyingKey::deserialize_uncompressed_unchecked(&mut g16_vk_serialized.as_slice()).unwrap();
-        
-        let end = get_current_time_in_millis();
-        let elapsed = end - start;
-        // alert("deserialized g16_vk");
-        //  web_sys::console::log_1(&format!("g16_vk_deserialized: {:?}", g16_vk_deserialized).into());
-         web_sys::console::log_1(&format!("deserialized g16_vk: {:?}", elapsed).into());
-
-    let start = get_current_time_in_millis();
-        let g16_pk_deserialized: ProvingKey<Bn254> = ProvingKey::deserialize_uncompressed_unchecked(&mut g16_pk_serialized.as_slice()).unwrap();
-        
-        let end = get_current_time_in_millis();
-        let elapsed = end - start;
-        // alert("deserialized g16_pk"); // runs up to here as of 9/12 11:14 PM
-        //  web_sys::console::log_1(&format!("g16_pk_deserialized: {:?}", g16_pk_deserialized).into());
-         web_sys::console::log_1(&format!("deserialized g16_pk: {:?}", elapsed).into());
-
-            // let pp_hash = nova_vp.pp_hash();
-            // let pp = (g16_pk, nova_pp.cs_pp);
-            // let vp = (pp_hash, g16_vk, nova_vp.cs_vp);
-            // Ok((pp, vp))
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     let start = get_current_time_in_millis();
         let nova_params = (nova_pp_deserialized, nova_vp_deserialized);
@@ -282,36 +251,6 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
         let elapsed = end - start;
         // alert("initialized nova from the deserialized parameters"); // WORKS IN BROWSER UP TO HERE AS OF 9/13 10:44 PM
          web_sys::console::log_1(&format!("initialized nova: {:?}", elapsed).into());
-
-    // // TODO - find cleaner way to avoid ownership issues
-    // // (problem is functions expect certain types D: so it's not immediately &s everywhere)
-
-    // let (decider_pp, decider_vp) = D::preprocess(&mut rng, &nova_params, nova.clone()).unwrap();
-
-    let start = get_current_time_in_millis();
-        let decider_pp = (g16_pk_deserialized, (nova_params.0).cs_pp);
-        
-        let end = get_current_time_in_millis();
-        let elapsed = end - start;
-        // alert("prepared decider_pp!");
-        //  web_sys::console::log_1(&format!("decider_pp: {:?}", decider_pp).into());
-         web_sys::console::log_1(&format!("prepared decider_pp: {:?}", elapsed).into());
-
-    let start = get_current_time_in_millis();
-        let pp_hash = (nova_params.1).pp_hash().unwrap();
-        let decider_vp: VerifierParam<G1, 
-                <KZG<'static, Bn254> as CommitmentScheme<G1>>::VerifierParams, 
-                <Groth16<Bn254> as ark_snark::SNARK<Fr>>::VerifyingKey> = VerifierParam {
-            pp_hash,
-            snark_vp: g16_vk_deserialized,
-            cs_vp: (nova_params.1).cs_vp,
-        };
-        
-        let end = get_current_time_in_millis();
-        let elapsed = end - start;
-        // alert("prepared decider_vp!");
-        //  web_sys::console::log_1(&format!("decider_vp: {:?}", decider_vp).into());
-         web_sys::console::log_1(&format!("prepared decider_vp: {:?}", elapsed).into());
 
     // run n steps of the folding iteration
     let start_outer = get_current_time_in_millis();
@@ -331,47 +270,9 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
         web_sys::console::log_1(&format!("finished folding: {:?}", elapsed).into());
 
     let start = get_current_time_in_millis();
-        // let proof = D::prove(rng.clone(), decider_pp, nova.clone()).unwrap();
-        // alert("generating decider proof....praying...");
-        // let result = D::prove(rng.clone(), decider_pp, nova.clone());
-        // let result = panic::catch_unwind(|| {
-        //     D::prove(rng.clone(), decider_pp, nova.clone()) // Prove should return a proof directly
-        // });
+        
+    // now serialize just the IVCProof
 
-        let proof_result = std::panic::catch_unwind(AssertUnwindSafe(|| {
-            D::prove(rng.clone(), decider_pp, nova.clone());
-            let end = get_current_time_in_millis();
-            let elapsed = end - start;
-        }));
-            match proof_result {
-                Ok(mut proof) => {
-                    // Proceed with `nova`
-                     web_sys::console::log_1(&format!("generated decider proof: {:?}", elapsed).into());
-                }
-                Err(e) => {
-                    // Use web_sys to log the error message to the browser console
-                     web_sys::console::log_1(&format!("Error initializing Nova: {:?}", e).into());
-                     web_sys::console::log_1(&format!("failed to generate decider proof: {:?}", elapsed).into());
-                }
-            }
-
-    // COMMENTING OUT EVERYTHING BELOW BECAUSE GETTING PROOF FROM D::prove DOESN'T WORK YET
-    // // now serialize the verifier_params, proof and public inputs to feed into the verifier-site
-
-    // // serialize the verifier_params
-    // let start = get_current_time_in_millis();
-    //     let mut decider_vp_serialized = vec![];
-    //     decider_vp
-    //         .serialize_compressed(&mut decider_vp_serialized)
-    //         .unwrap();
-
-    //     let end = get_current_time_in_millis();
-    //     let elapsed = end - start;
-    //     // alert(&format!("decider_vp_serialized: {:?}", decider_vp_serialized));
-    //     web_sys::console::log_1(&format!("decider_vp_serialized: {:?}", decider_vp_serialized).into());
-    //     web_sys::console::log_1(&format!("decider_vp_serialized: {:?}", elapsed).into());
-
-    // // serialize the proof
     // let start = get_current_time_in_millis();
     //     let mut proof_serialized = vec![];
     //     proof.serialize_compressed(&mut proof_serialized).unwrap();
@@ -380,31 +281,6 @@ pub fn frog_nova(r1cs_bytes: Vec<u8>,
     //     // alert(&format!("proof_serialized: {:?}", proof_serialized));
     //     web_sys::console::log_1(&format!("proof_serialized: {:?}", proof_serialized).into());
     //     web_sys::console::log_1(&format!("proof_serialized: {:?}", elapsed).into());
-
-    // // serialize the public inputs in a single packet
-    // let start = get_current_time_in_millis();
-    //     let mut public_inputs_serialized = vec![];
-    //     nova.i
-    //         .serialize_compressed(&mut public_inputs_serialized)
-    //         .unwrap();
-    //     nova.z_0
-    //         .serialize_compressed(&mut public_inputs_serialized)
-    //         .unwrap();
-    //     nova.z_i
-    //         .serialize_compressed(&mut public_inputs_serialized)
-    //         .unwrap();
-    //     nova.U_i
-    //         .serialize_compressed(&mut public_inputs_serialized)
-    //         .unwrap();
-    //     nova.u_i
-    //         .serialize_compressed(&mut public_inputs_serialized)
-    //         .unwrap();
-
-    //     let end = get_current_time_in_millis();
-    //     let elapsed = end - start;
-    //         // alert(&format!("public_inputs_serialized: {:?}", public_inputs_serialized));
-    //     web_sys::console::log_1(&format!("public_inputs_serialized: {:?}", public_inputs_serialized).into());
-    //     web_sys::console::log_1(&format!("public_inputs_serialized: {:?}", elapsed).into());
 
     let end_total = get_current_time_in_millis();
     let elapsed_total = end_total - start_total;
