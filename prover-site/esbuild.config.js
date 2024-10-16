37e68dcf8fbc9b28669f7c3f571a39a66167b736
janabel <janabeltxia@gmail.com>
2024-10-16 14:40:06 -0400

    idk with esbuild config

--- prover-site/esbuild.config.js
+++ prover-site/esbuild.config.js
@@ -1,12 +1,12 @@
 import esbuild from "esbuild";
-import nodeGlobalsPolyfill from "@esbuild-plugins/node-globals-polyfill";
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 esbuild
   .build({
     entryPoints: ["src/main.tsx"], // Adjust as necessary
     bundle: true,
     outfile: "dist/bundle.js", // Specify output path
-    plugins: [nodeGlobalsPolyfill()],
+    plugins: [NodeGlobalsPolyfillPlugin()],
     define: {
       "process.env.NODE_ENV": '"production"', // For environment variables
     },
